3b0fd02ff92229a2e3278421ffcbf6ae057056d4
ZinLinnhtet <mackk5504@gmail.com>
2024-08-10 21:15:44 +0630

    -

--- DotNet8.WebApi/Models/BlogDataModel.cs
+++ DotNet8.WebApi/Models/BlogDataModel.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.WebApi.Models
+﻿namespace DotNet8.WebApi.Models;
+
+public class BlogDataModel
 {
-    public class BlogDataModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
