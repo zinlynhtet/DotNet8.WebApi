9dfbfd0ebcdfc389c897c9c487a2f5ed0e2b23aa
ZinLinnhtet <mackk5504@gmail.com>
2024-08-10 21:15:32 +0630

    -

--- DotNet8.WebApi/BaseController.cs
+++ DotNet8.WebApi/BaseController.cs
@@ -1,44 +1,39 @@
-﻿using AspNetCore.Reporting;
-using DotNet8.WebApi.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace DotNet8.WebApi;
 
-namespace DotNet8.WebApi
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public BaseController(IWebHostEnvironment webHostEnvironment)
     {
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        _webHostEnvironment = webHostEnvironment;
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+    }
 
-        public BaseController(IWebHostEnvironment webHostEnvironment)
+    protected IActionResult ExportReport<T>(ReportModel<T> requestModel)
+    {
+        string mimetype = "";
+        int extension = 1;
+        //var path = $"{this._webHostEnvironment.WebRootPath}\\ReportFiles\\{requestModel.ReportFileName}.rdlc";
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ReportFiles", requestModel.ReportFileName + ".rdlc");
+        LocalReport lr = new LocalReport(path);
+        lr.AddDataSource(requestModel.DataSetName, requestModel.DataLst);
+
+        if (requestModel.ReportType == EnumFileType.Pdf)
         {
-            _webHostEnvironment = webHostEnvironment;
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            ReportResult result = lr.Execute(RenderType.Pdf, extension,
+                requestModel.Parameters, mimetype);
+            return File(result.MainStream, "application/pdf", $"{requestModel.ExportFileName}.pdf");
+        }
+        else if (requestModel.ReportType == EnumFileType.Excel)
+        {
+            ReportResult result = lr.Execute(RenderType.Excel, extension,
+                requestModel.Parameters, mimetype);
+            return File(result.MainStream, "application/msexcel", $"{requestModel.ExportFileName}.xls");
         }
 
-        public IActionResult ExportReport<T>(ReportModel<T> requestModel)
-        {
-            string mimetype = "";
-            int extension = 1;
-            //var path = $"{this._webHostEnvironment.WebRootPath}\\ReportFiles\\{requestModel.ReportFileName}.rdlc";
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ReportFiles", requestModel.ReportFileName + ".rdlc");
-            LocalReport lr = new LocalReport(path);
-            lr.AddDataSource(requestModel.DataSetName, requestModel.DataLst);
-
-            if (requestModel.ReportType == EnumFileType.Pdf)
-            {
-                ReportResult result = lr.Execute(RenderType.Pdf, extension,
-                                        requestModel.Parameters, mimetype);
-                return File(result.MainStream, "application/pdf", $"{requestModel.ExportFileName}.pdf");
-            }
-            else if (requestModel.ReportType == EnumFileType.Excel)
-            {
-                ReportResult result = lr.Execute(RenderType.Excel, extension,
-                                        requestModel.Parameters, mimetype);
-                return File(result.MainStream, "application/msexcel", $"{requestModel.ExportFileName}.xls");
-            }
-
-            return Ok();
-        }
+        return Ok();
     }
 }
