aa6cb7032bc8f57affeaa0506451edb1a92e59bf
ZinLinnhtet <mackk5504@gmail.com>
2024-08-10 21:16:00 +0630

    -

--- DotNet8.WebApi/Program.cs
+++ DotNet8.WebApi/Program.cs
@@ -1,19 +1,12 @@
-using DotNet8.WebApi;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
